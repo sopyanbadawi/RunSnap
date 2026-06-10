2752e174ff861528ec2ee3b00a9bc448e514fb21
sopyanbadawi <sopyanbadawi@gmail.com>
2026-06-10 20:00:27 +0700

    fix: add Authenticate middleware to Filament admin panel provider

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -72,6 +72,7 @@ class AdminPanelProvider extends PanelProvider
                 DispatchServingFilamentEvent::class,
             ])
             ->authMiddleware([
+                Authenticate::class,
                 CheckRole::class,
             ]);
     }
